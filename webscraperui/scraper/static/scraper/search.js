55304d5c0fa84550bb90cd1fba923f0b64a9fb75
Ethan John <97091148+johnmanjohnston@users.noreply.github.com>
2022-09-15 09:37:12 -0700

    Implemented pagination/refactors

--- webscraperui/scraper/static/scraper/search.js
+++ webscraperui/scraper/static/scraper/search.js
@@ -1,4 +1,41 @@
 const url = new URL(location.href);
 const urlsearchparams = new URLSearchParams(url.search);
-const searchbar = document.getElementById("search-bar");
-searchbar.setAttribute("value", urlsearchparams.get("q"));
+const searchBar = document.getElementById("search-bar");
+searchBar.setAttribute("value", urlsearchparams.get("q"));
+
+const prevPage = document.getElementById("prev-page");
+const nextPage = document.getElementById("next-page");
+var targetPage;
+
+if (!urlsearchparams.get("pg") || parseInt(urlsearchparams.get("pg")) == 0) {
+    prevPage.style.display = "none";
+    targetPage = 0;
+}
+
+if (urlsearchparams.get("pg")) {
+    targetPage = parseInt(urlsearchparams.get("pg"));
+}
+
+function updatePage() {
+    // console.log("updatePage() called");
+    
+    if (!urlsearchparams.get("pg")) {
+        urlsearchparams.append("pg", targetPage);
+    } else {
+        urlsearchparams.set("pg", targetPage);
+    }
+
+    window.location.search = urlsearchparams;
+}
+
+prevPage.onclick = () => {
+    targetPage--;
+    updatePage();
+}
+
+nextPage.onclick = () => {
+    targetPage++;
+    updatePage();
+}
+
+// i know. code's messy af but get's the job done and that's what matters.
